a93e42852e357e99b663caff5b7bc33245863ac4
Alexander Jensby Thomsen <alexander.thomsen@aalborgforsyning.dk>
2025-01-31 13:29:47 +0100

    Bug fix for createconnection in DB.py

--- Backend/Database/db.py
+++ Backend/Database/db.py
@@ -1,6 +1,7 @@
 import pyodbc
 import os
 from dotenv import load_dotenv
+load_dotenv()
 
 class database:
     def create_connection():
@@ -8,11 +9,11 @@ class database:
             conn_str = os.getenv('DB_CONNECTION_STRING')
 
             cnxn = pyodbc.connect(conn_str)
-            cursor = cnxn.cursor
+            cursor = cnxn.cursor()
             
             return cursor
         except Exception as e:
-            print(f"Failed to connect to database!{e}")
+            print(f"Failed to connect to database! {e}")
             return None
 
     def close_connection(cnxn, cursor):
